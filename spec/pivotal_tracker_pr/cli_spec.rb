c17195d56ac2e7e60b356cfb4c627d13a52e043f
blueplanet <erguolinge@gmail.com>
2016-06-15 06:06:25 +0900

    Add spec

---
+++ spec/pivotal_tracker_pr/cli_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe PivotalTrackerPr::CLI do
+  before do
+    work_path = Dir.mktmpdir
+    Dir.chdir work_path
+
+    system 'git init; touch test.rb; git add .; git commit -m init'
+    system 'git checkout -b test-111'
+  end
+
+  let(:cli) { PivotalTrackerPr::CLI.new }
+
+  describe '#create' do
+    subject { cli.create story_id }
+
+    context 'story_id is nil' do
+      let(:story_id) { nil }
+      it 'should be call say method' do
+        expect(cli).to receive(:get_story_name).with('111').and_return('111 name')
+        expect(cli).to receive(:write_pull_request_template).with('111', '111 name')
+        subject
+      end
+    end
+
+    context 'story_id is present'  do
+      let(:story_id) { 123 }
+    end
+  end
+end
