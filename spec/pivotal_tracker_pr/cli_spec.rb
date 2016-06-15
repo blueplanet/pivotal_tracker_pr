27a4d3dae7bd0e60c3ff62ec0fd2b220ff2ca7bd
blueplanet <erguolinge@gmail.com>
2016-06-16 06:10:41 +0900

    use stub

--- spec/pivotal_tracker_pr/cli_spec.rb
+++ spec/pivotal_tracker_pr/cli_spec.rb
@@ -1,36 +1,63 @@
 require 'spec_helper'
 
 RSpec.describe PivotalTrackerPr::CLI do
-  before do
-    work_path = Dir.mktmpdir
-    Dir.chdir work_path
-
-    system 'git init; touch test.rb; git add .; git commit -m init'
-    system 'git checkout -b test-111'
-
-    ENV['PT_TOKEN'] = 'PT_TOKEN'
-    ENV['PT_PROJECT_ID'] = 'PT_PROJECT_ID'
-  end
-
   let(:cli) { PivotalTrackerPr::CLI.new }
+  before { allow(cli).to receive(:say).and_return(true) }
 
   describe '#create' do
     subject { cli.create story_id }
 
     context 'story_id is nil' do
       let(:story_id) { nil }
-      it 'should be call say method' do
-        expect(cli).to receive(:get_story_name).with('111').and_return('111 name')
-        expect(cli).to receive(:write_pull_request_template).with('111', '111 name')
-        subject
+
+      context 'parsed story id is present' do
+        before { expect(cli).to receive(:parse_story_id).and_return('111') }
+
+        context 'get_story_name is success' do
+          before { expect(cli).to receive(:get_story_name).with('111').and_return('111 name') }
+
+          it 'should be call write_pull_request_template' do
+            expect(cli).to receive(:check_env_vars)
+            expect(cli).to receive(:write_pull_request_template)
+            expect(cli).to receive(:system).with('hub pull-request --browse')
+
+            subject
+          end
+        end
+
+        context 'get_story_name is fail' do
+          before { expect(cli).to receive(:get_story_name).with('111').and_return(nil) }
+
+          it 'should not to be call write_pull_request_template' do
+            expect(cli).to receive(:check_env_vars)
+            expect(cli).to_not receive(:write_pull_request_template)
+            expect(cli).to receive(:system).with('hub pull-request --browse')
+
+            subject
+          end
+        end
+      end
+
+      context 'parsed story id is nil' do
+        before { expect(cli).to receive(:parse_story_id).and_return(nil) }
+
+        it 'should be only call hub command' do
+          expect(cli).to_not receive(:check_env_vars)
+          expect(cli).to_not receive(:get_story_name)
+          expect(cli).to receive(:system).with('hub pull-request --browse')
+
+          subject
+        end
       end
     end
 
     context 'story_id is present'  do
       let(:story_id) { '123' }
-      it 'should be call say method' do
+      it 'should be call get_story_name && hub command' do
         expect(cli).to receive(:get_story_name).with('123').and_return('123 name')
         expect(cli).to receive(:write_pull_request_template).with('123', '123 name')
+        expect(cli).to receive(:system).with('hub pull-request --browse')
+
         subject
       end
     end
